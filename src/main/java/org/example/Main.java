1fdbd2630ffcbaf18706eaf13fe196f99ac34c19
Craig Raw <craigraw@gmail.com>
2023-07-06 14:11:27 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import net.sourceforge.zbar.ZBar;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+
+        BufferedImage bufferedImage = ImageIO.read(new File("/Users/scy/git/zbar-java/screenshot.png"));
+
+        if(ZBar.isEnabled()) {
+            long start = System.currentTimeMillis();
+            ZBar.Scan scan = ZBar.scan(bufferedImage);
+            System.out.println(System.currentTimeMillis() - start);
+            if(scan != null) {
+                System.out.println(scan.stringData());
+            }
+        }
+
+        if(ZBar.isEnabled()) {
+            long start = System.currentTimeMillis();
+            ZBar.Scan scan = ZBar.scan(bufferedImage);
+            System.out.println(System.currentTimeMillis() - start);
+            if(scan != null) {
+                System.out.println(scan.stringData());
+            }
+        }
+    }
+}
